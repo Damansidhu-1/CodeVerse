63ac9783091b38d25fb9bc57c6db5517e887400b
Damandeep Singh <deepdaman46@gmail.com>
2024-04-10 17:22:49 +0530

    OTP via email

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const OTPSchema = new mongoose.Schema({
     
@@ -17,5 +18,24 @@ const OTPSchema = new mongoose.Schema({
     }
     
 });
+
+// a function to send email
+async function sendVerificationMail(email , otp) {
+    try {
+        
+        const mailResponse = await mailSender(email , "verification email from CodeVerse " ,otp);
+        console.log('email sent succesfully' , mailResponse );
+
+    } catch (error) {
+        console.log("error occured while sending email");
+        throw error
+
+    }
+}
+
+OTPSchema.pre("save" , async function(next) {
+    await sendVerificationMail(this.email , this.otp);
+    next();
+} )
 
 module.exports = mongoose.model("OTP" , OTPSchema );
---
+++ server/utils/mailSender.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+
+const mailSender = async (email , title , body)=> {
+    try {
+        let transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            }
+        });
+
+        // sending mail
+        let info = await transporter.sendMail({
+            from:"CODE || VERSE - by DAMAN",
+            to:`${email}`,
+            subject: `${title}`,
+            html: `${body}`,
+        })
+        console.log(info);
+        return info;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = mailSender;
