a1cf7da770396b0f366bbba8874a7ff6c0b3c767
Damandeep Singh <deepdaman46@gmail.com>
2024-04-15 14:04:00 +0530

    explore section modification

--- src/components/core/HomePage/ExploreMore.jsx
+++ src/components/core/HomePage/ExploreMore.jsx
@@ -27,42 +27,43 @@ const ExploreMore = () => {
     }
 
     return (
-        <div className="relative">
-            
-            <div className="text-4xl font-semibold text-center" >
-                Unlock the 
-                <HighlightText text={"Power of Code"} />
+        <div>
+            {/* Explore more section */}
+            <div>
+                <div className="text-4xl font-semibold text-center my-10">
+                    Unlock the
+                    <HighlightText text={"Power of Code"} />
+                    <p className="text-center text-richblack-300 text-lg font-semibold mt-1">
+                    Learn to Build Anything You Can Imagine
+                    </p>
+                </div>
             </div>
 
-            <p className="text-[16px] text-center text-richblack-300  mt-3 ">
-                Learn to Build Anything You Can Imagine
-            </p>
-
             
-            <div className="flex gap-2 items-center rounded-full bg-richblack-800 mb-5 mt-5 px-1 py-1">
+            <div className="hidden lg:flex gap-5 -mt-5 mx-auto w-max bg-richblack-800 text-richblack-200 p-1 rounded-full font-medium drop-shadow-[0_1.5px_rgba(255,255,255,0.25)]">
                 {
                     tabsName.map((element , index ) => {
                         return (
                             <div
                                 key={index}
                                 className=
-                                {` text-[16px] flex gap-2 items-center 
-                                ${currentTab === element ? "bg-richblack-900 text-richblack-5 font-medium " : "text-richblack-200"} rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5 px-7 py-2 `}
+                                {` text-[16px] flex flex-row gap-2 items-center 
+                                ${currentTab === element ? "bg-richblack-900 text-richblack-5 font-medium " : "text-richblack-200"} rounded-full transition-all duration-200 cursor-pointer hover:bg-richblack-900 hover:text-richblack-5 px-7 py-[7px] `}
                                 onClick={() => setMyCard(element) }
                             >
-                                <button>
-                                    {element}
-                                </button>
+                                
+                                {element}
+                                
                             </div>
                         )
                     })
                 }
             </div>
 
-            <div className="lg:h-[150px]"></div>
+            <div className="hidden lg:block lg:h-[200px]"></div>
             
             {/* course card da group */}
-            <div className="absolute flex flex-row gap-10 justify-between w-full">
+            <div className="lg:absolute gap-10 justify-center lg:gap-0 flex lg:justify-between flex-wrap w-full lg:bottom-[0] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[50%] text-black lg:mb-0 mb-7 lg:px-0 px-3">
                 {
                     courses.map( (element , index) => {
                         return (
