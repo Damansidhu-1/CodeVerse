0fca1a90faf35aafb5c20bc1cf70a6e702b5f77e
Damandeep Singh <deepdaman46@gmail.com>
2024-07-25 12:40:49 +0530

    modified routes

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -8,21 +8,23 @@ const mailSender = async (email , title , body)=> {
             auth:{
                 user:process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
-            }
+            },
+            secure: false,
         });
 
         // sending mail
         let info = await transporter.sendMail({
-            from:"CODE || VERSE - by DAMAN",
-            to:`${email}`,
-            subject: `${title}`,
-            html: `${body}`,
-        })
+            from: `"CodeVerse " <${process.env.MAIL_USER}>`, // sender address
+            to: `${email}`, // list of receivers
+            subject: `${title}`, // Subject line
+            html: `${body}`, // html body
+          })
         console.log(info);
         return info;
 
     } catch (error) {
         console.error(error);
+        return error.message
     }
 }
 
